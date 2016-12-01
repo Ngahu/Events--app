67e70e38574f3e630d9658c172159e709b114291
Ngahu <jamaalaraheem@gmail.com>
2016-12-02 02:56:08 +0300

    creating of the models incharge of creating of a new event

--- mysite/Event/models.py
+++ mysite/Event/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Create(models.Model):
+    """incharge of creating of the events"""
+    title = models.CharField(max_length=200)
+    Location = models.CharField(max_length=100)
+    Event_type = models.CharField(max_length=300)
+    Event_topic = models.CharField(max_length=250)
+    Event_image = models.FileField(null=False, blank=False)
+    Event_description = models.CharField(max_length=1000)
+    Organizer_name = models.CharField(max_length=100,null=False)
+    updated = models.DateTimeField(auto_now=True,auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False,auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
+
+
+
