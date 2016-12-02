abb6867c3e3426b9bb774b438e0c8b3aa457461d
Ngahu <jamaalaraheem@gmail.com>
2016-12-02 03:09:03 +0300

    adding of the creating of the models

--- mysite/Event/models.py
+++ mysite/Event/models.py
@@ -2,10 +2,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Create(models.Model):
+class CreateEvent(models.Model):
     """incharge of creating of the events"""
     title = models.CharField(max_length=200)
     Location = models.CharField(max_length=100)
+    time = models.CharField(max_length=40)
     Event_type = models.CharField(max_length=300)
     Event_topic = models.CharField(max_length=250)
     Event_image = models.FileField(null=False, blank=False)
@@ -13,6 +14,7 @@ class Create(models.Model):
     Organizer_name = models.CharField(max_length=100,null=False)
     updated = models.DateTimeField(auto_now=True,auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False,auto_now_add=True)
+
 
     def __unicode__(self):
         return self.title
