1449b843ddd38150fba57a36d7a3c0fbfceac239
Ngahu <jamaalaraheem@gmail.com>
2016-12-02 13:31:40 +0300

    creating the right url mapping to the create view

--- mysite/Event/urls.py
+++ mysite/Event/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
 
     #/homepage/event-details
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    url(r'Event/add/$',views.EventCreate.as_view(), name='Event-add'),
 ]
