479385e43f7ecccd34acf6d102131684f2b53f75
Ngahu <jamaalaraheem@gmail.com>
2016-12-05 10:51:53 +0300

    creating of the update and delete urls

--- mysite/Event/urls.py
+++ mysite/Event/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
     url(r'Event/add/$',views.EventCreate.as_view(), name='Event-add'),
+
+    url(r'Event/(?P<pk>[0-9]+)/$', views.EventUpdate.as_view(), name='Event-update'),
+
+    url(r'Event/(?P<pk>[0-9]+)/delete/$', views.EventDelete.as_view(), name='Event-delete'),
+
 ]
