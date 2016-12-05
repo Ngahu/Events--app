ad3752dfa6d59817f6136ecd1f7b99f8e48ab200
Ngahu <jamaalaraheem@gmail.com>
2016-12-05 10:20:55 +0300

    making the right url pattern to the media root

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^',include('Event.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    """to check if debug is on """
+    urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
